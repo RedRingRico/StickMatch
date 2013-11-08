2b97984cc0f85e6cf9d27ab2c9f03a31f189aabe
Rico <teh_programerer@interactivelaboratories.com>
2013-11-08 17:01:41 +0000

    |SOURCE| [Common::Game] * Pointer is warped to the centre of the screen and kept there * Pointer is hidden and shown on program exit * Holding down mouse button 2 will allow for looking with the mouse * Position set to 100 units back and 100 units up

--- Source/Common/Source/Game.cpp
+++ Source/Common/Source/Game.cpp
@@ -6,6 +6,8 @@
 const ZED_FLOAT32 THRUST_SPEED = 0.2f;
 const ZED_FLOAT32 STRAFE_SPEED = 0.2f;
 const ZED_FLOAT32 ELEVATE_SPEED = 0.2f;
+const ZED_FLOAT32 YAW_SPEED = 0.02f;
+const ZED_FLOAT32 PITCH_SPEED = 0.02f;
 
 namespace StickMatch
 {
@@ -56,7 +58,13 @@ namespace StickMatch
 			&Projection );
 		ZED::Arithmetic::Vector3 CameraVelocity( 0.0f, 0.0f, 0.0f );
 		ZED::Arithmetic::Vector3 CameraRotation( 0.0f, 0.0f, 0.0f );
-		m_DebugCamera.Position( CameraVelocity );
+		m_DebugCamera.Position(
+			ZED::Arithmetic::Vector3( 0.0f, 100.0f, 100.0f ) );
+		ZED_UINT32 HalfWidth = 1280 / 2;
+		ZED_UINT32 HalfHeight = 720 / 2;
+		m_pWindow->WarpPointer( HalfWidth, HalfHeight );
+		ZED_UINT32 MouseX = 0, MouseY = 0;
+		m_pWindow->HideCursor( );
 
 		while( m_Running )
 		{
@@ -102,6 +110,29 @@ namespace StickMatch
 				CameraVelocity[ 1 ] = -ELEVATE_SPEED;
 			}
 
+			m_Mouse.Position( &MouseX, &MouseY );
+
+			if( m_Mouse.IsButtonDown( ZED_MOUSE_BUTTON_2 ) )
+			{
+				if( MouseX < HalfWidth )
+				{
+					CameraRotation[ 1 ] = YAW_SPEED;
+				}
+				if( MouseX > HalfWidth )
+				{
+					CameraRotation[ 1 ] = -YAW_SPEED;
+				}
+
+				if( MouseY < HalfHeight )
+				{
+					CameraRotation[ 0 ] = PITCH_SPEED;
+				}
+				if( MouseY > HalfHeight )
+				{
+					CameraRotation[ 0 ] = -PITCH_SPEED;
+				}
+			}
+
 			m_DebugCamera.Move( CameraVelocity );
 			m_DebugCamera.Rotate( CameraRotation );
 			
@@ -110,7 +141,10 @@ namespace StickMatch
 
 			CameraVelocity.Zero( );
 			CameraRotation.Zero( );
+			m_pWindow->WarpPointer( HalfWidth, HalfHeight );
 		}
+
+		m_pWindow->ShowCursor( );
 
 		return ZED_OK;
 	}
